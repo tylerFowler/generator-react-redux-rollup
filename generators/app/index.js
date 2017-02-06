c82b935a0cc4b36b7d650ff54f74481b4c7ad232
Tyler Fowler <tylerfowler.1337@gmail.com>
2017-02-05 21:45:27 -0600

    Add dotfiles & project boilerplate templates

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,6 +1,7 @@
-var Generator = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
+/* eslint no-unused-vars:0 */
+const Generator = require('yeoman-generator');
+const chalk     = require('chalk');
+const yosay     = require('yosay');
 
 module.exports = class GeneratorReactReduxRollup extends Generator {
   constructor(args, opts) {
@@ -8,33 +9,79 @@ module.exports = class GeneratorReactReduxRollup extends Generator {
   }
 
   prompting() {
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the splendid ' + chalk.red('generator-react-redux-rollup') + ' generator!'
-    ));
+    const prompts = [
+      {
+        type: 'input',
+        name: 'appName',
+        message: "What's your application name?",
+        default: utils.yeoman.getAppName()
+      }
+    ];
 
-    // TODO: do we even need any props?
-    const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
+    return this.prompt(prompts).then(props => this.props = props);
   }
 
   writing() {
+    this.writeDotfiles();
+  }
+
+  install() {
+    this.installDependencies({ bower: false });
+  }
+
+  writeDotfiles() {
+    this.fs.copy(
+      this.templatePath('.editorconfig'),
+      this.destinationPath('.editorconfig')
+    );
+
     this.fs.copy(
       this.templatePath('.babelrc'),
       this.destinationPath('.babelrc')
     );
+
+    this.fs.copy(
+      this.templatePath('.eslintrc'),
+      this.destinationPath('.eslintrc')
+    );
   }
 
-  install() {
-    this.installDependencies();
+  writeProjectBoilerplate() {
+    this.fs.copy(
+      this.templatePath('app/index.js'),
+      this.destinationPath('app/index.js')
+    );
+
+    this.fs.copy(
+      this.templatePath('app/App.js'),
+      this.destinationPath('app/components/App.js')
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('index.js'),
+      this.destinationPath('public/index.js'),
+      { title: this.props.appName }
+    );
+
+    // create Redux directories
+    this.fs.copy(
+      this.templatePath('.gitkeep'),
+      this.destinationPath('app/actions/.gitkeep')
+    );
+
+    this.fs.copy(
+      this.templatePath('.gitkeep'),
+      this.destinationPath('app/constants/.gitkeep')
+    );
+
+    this.fs.copy(
+      this.templatePath('.gitkeep'),
+      this.destinationPath('app/containers/.gitkeep')
+    );
+
+    this.fs.copy(
+      this.templatePath('.gitkeep'),
+      this.destinationPath('app/reducers/.gitkeep')
+    );
   }
 };
